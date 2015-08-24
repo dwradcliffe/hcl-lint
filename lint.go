4de21f15be03f2f604cbd29e60074f7a762b8a42
David Radcliffe <radcliffe.david@gmail.com>
2015-08-24 13:41:00 -0400

    initial commit of basic lint tool

---
+++ lint.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/hashicorp/hcl"
+	"github.com/mitchellh/colorstring"
+)
+
+func main() {
+	for i, arg := range os.Args {
+		if i == 0 {
+			continue
+		}
+		search := arg
+		if info, err := os.Stat(arg); err == nil && info.IsDir() {
+			search = fmt.Sprintf("%s/*.tf", arg)
+		}
+		files, err := filepath.Glob(search)
+		if err != nil {
+			colorstring.Printf("[red]Error finding files: %s", err)
+		}
+		for _, filename := range files {
+			fmt.Printf("Checking %s ... ", filename)
+			file, err := ioutil.ReadFile(filename)
+			if err != nil {
+				colorstring.Printf("[red]Error reading file: %s\n", err)
+				break
+			}
+			_, err = hcl.Parse(string(file))
+			if err != nil {
+				colorstring.Printf("[red]Error parsing file: %s\n", err)
+				break
+			}
+			colorstring.Printf("[green]OK!\n")
+		}
+	}
+
+}
